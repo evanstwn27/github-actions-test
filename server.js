92a924cbdb9cb0b304b6334a3e624b8a8b5ed956
Christopher Evan <Christopher.Evan@infra.core>
2025-05-08 20:45:23 +0700

    update message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! I am using Github Actions for CI/CD.');
+  res.send('Hello World! Github Actions is awesome!');
 });
 
 app.get('/info', (req, res) => {
