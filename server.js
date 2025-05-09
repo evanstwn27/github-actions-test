514a19eb4ec7c91eacfd85b642845f101b20e7d9
Christopher Evan <Christopher.Evan@infra.core>
2025-05-09 10:06:03 +0700

    update workflow

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Tes Server Guido 3');
+  res.send('Hello World! Tes Server Guido 4');
 });
 
 app.get('/info', (req, res) => {
