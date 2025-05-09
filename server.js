6f5f740ab7957ad65319af5b7db7daf28346fad8
Christopher Evan <Christopher.Evan@infra.core>
2025-05-09 10:38:02 +0700

    test 4

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Tes Multiple Servers 3');
+  res.send('Hello World! Tes Multiple Servers 4');
 });
 
 app.get('/info', (req, res) => {
