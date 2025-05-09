7dcd30dd27eada31abf0f7dc497caa456e2b3c34
Christopher Evan <Christopher.Evan@infra.core>
2025-05-09 09:30:11 +0700

    test server guido

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Test to on prem server');
+  res.send('Hello World! Tes Server Guido');
 });
 
 app.get('/info', (req, res) => {
