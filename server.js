dbd90a3ca5adf768eda49e43dec46f2c8ef6f9d8
Christopher Evan <Christopher.Evan@infra.core>
2025-05-09 09:06:15 +0700

    testing to on premise server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Test 2');
+  res.send('Hello World! Test to on prem server');
 });
 
 app.get('/info', (req, res) => {
